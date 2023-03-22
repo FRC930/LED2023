0558fac67ed3a59f4cb7c1647d423399a62eb47c
Hussain Mehdi <hus23337@gmail.com>
2023-03-21 20:32:48 -0500

    (3/21 20:31) Fix Bouncing and Fading Cone pattern
    Fix the bouncing cone pattern and the fading cone request patterns.

--- Combined_Sequence/Combined_Sequence.ino
+++ Combined_Sequence/Combined_Sequence.ino
@@ -1,7 +1,7 @@
 #include <FastLED.h>
 // Create 4 array's of LED's with 81 LEDs each
-#define NUM_STRIPS 3
-#define NUM_LEDS_PER_STRIP 36
+#define NUM_STRIPS 4
+#define NUM_LEDS_PER_STRIP 28
 CRGB leds[NUM_STRIPS][NUM_LEDS_PER_STRIP];
 CRGB redStrip[NUM_LEDS_PER_STRIP];
 
@@ -10,15 +10,15 @@ CRGB redStrip[NUM_LEDS_PER_STRIP];
 #define PIN_ACTIVE 1
 #define PIN_INACTIVE 0
 
-  // Intialize random numbers for random LED pattern
+// Intialize random numbers for random LED pattern
 long randNumber1;
 long randNumber2;
 long randNumber3;
 
-  // Setup the different strips of LEDs
-  void setup() {
+// Setup the different strips of LEDs
+void setup() {
   // Set the brightness of the LED Strips in order to avoid overloading the board
-  FastLED.setBrightness(75);  
+  FastLED.setBrightness(100);  //50);
   // tell FastLED library what pin the LED strips are on
   FastLED.addLeds<NEOPIXEL, 12>(leds[0], NUM_LEDS_PER_STRIP);
   // Define which input pins to pull the DIO value of the RIO From
@@ -36,6 +36,7 @@ void CLEAR() {
 }
 void REDALLIANCE() {
   FastLED.setBrightness(100);
+
   for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
     leds[0][i] = CRGB(255, 0, 0);
     FastLED.show();
@@ -52,57 +53,110 @@ void CONEREQUEST() {
 }
 void CONEREQUEST2() {
   static int position = 0;
+
   FastLED.setBrightness(100);
+
   for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
     leds[0][i] = CRGB(230, 100, 1);
   }
+
   position++;
   if (position >= NUM_LEDS_PER_STRIP) {
     position = 0;
   }
-  // FastLED.setBrightness(200);
+  FastLED.setBrightness(200);
   leds[0][position] = CRGB(230, 100, 1);
   FastLED.show();
 }
 void CONEREQUEST3() {
   static int position = 0;
   static bool reachedTop = false;
-  FastLED.setBrightness(100);
+  
   for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-    leds[0][i] = CRGB(230, 100, 1);
+    leds[0][i] = CRGB(0, 0, 0);
   }
-  if (!reachedTop) {
-    position++;
-  }
-  else {
-    position--;
-  }
-  if (position >= NUM_LEDS_PER_STRIP) {
-    reachedTop = true;
-  }
-  if (position <= 0) {
-    reachedTop = false;
-  }
-  // FastLED.setBrightness(200);
-  leds[0][position] = CRGB(230, 100, 1);
-  FastLED.show();
+
+  FastLED.setBrightness(80);
+  delay(10);
+    if (!reachedTop) {
+      position++;
+    } else {
+      position--;
+    }
+
+    if (position >= NUM_LEDS_PER_STRIP) {
+      reachedTop = true;
+    }
+    if (position <= 0) {
+      reachedTop = false;
+    }
+    
+    for(int i = position - 2; i < position + 2; i++) {
+      leds[0][i] = CRGB(230, 100, 1);
+    }
+    FastLED.show();
+    leds[0][position] = CRGB(0, 0, 0);
 }
 void CONEREQUEST4() {
-  FastLED.setBrightness(100);
-  for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-    leds[0][i] = CRGB(230, 100, 1);
+  //FastLED.setBrightness(80);
+  static int counter = 0;
+  static bool firstTime = true;
+  
+  //if(firstTime){
+    for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+      leds[0][i] = CRGB(230, 100, 1);
+    }
+    //firstTime = false;
+  //}
+
+  FastLED.show();
+
+  for(int i = 80; i > 0; i--){
+    FastLED.setBrightness(i);
+    FastLED.show();
+    delay(10);
   }
-  for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-    leds[0][i].fadeToBlackBy(64);
+
+  for(int i = 0; i < 80; i++){
+    FastLED.setBrightness(i);
+    FastLED.show();
+    delay(10);
   }
-  for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-    leds[0][i].fadeLightBy(256);
-  }
+
+  // for(int i = 0; i < 255; i++){
+  //   delay(100);
+  //   for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+  //     leds[0][i].fadeToBlackBy(i);
+  //   }
+  // }
+  
+  // for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+  //   leds[0][i].fadeToBlackBy(1);
+  // }
+
+  // for(int i = 255; i > 0; i--){
+  //   for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+  //     leds[0][i].fadeToBlackBy(i);
+  //   }
+  // }
+  // for(int i = 255; i > 0; i--){
+  //   delay();
+  //   for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+  //     leds[0][i].fadeToBlackBy(i);
+  //   }
+  // } 
+  // for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+  //   leds[0][i].fadeLightBy(256);
+  // }
+  
+
+  FastLED.show();
 }
 void CONEAQUIRED()
 // Set all the LEDs in the Array to Yellow
 {
   FastLED.setBrightness(100);
+
   for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
     leds[0][i] = CRGB(230, 100, 1);
     FastLED.show();
@@ -112,6 +166,7 @@ void CUBEREQUEST()
 // Set all the LEDs in the Array to Purple, wait half a second, and then clear all the LEDs and Repeat
 {
   FastLED.setBrightness(100);
+
   for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
     leds[0][i] = CRGB(20, 0, 25);
     FastLED.show();
@@ -121,43 +176,53 @@ void CUBEREQUEST()
 }
 void CUBEREQUEST2() {
   static int position = 0;
+
   FastLED.setBrightness(100);
+
   for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
     leds[0][i] = CRGB(20, 0, 25);
   }
+
   position++;
-  if (position >= NUM_LEDS_PER_STRIP) {
+
+    if (position >= NUM_LEDS_PER_STRIP) {
     position = 0;
   }
-  // FastLED.setBrightness(200);
+
+  FastLED.setBrightness(200);
   leds[0][position] = CRGB(20, 0, 25);
   FastLED.show();
 }
 void CUBEREQUEST3() {
   static int position = 0;
   static bool reachedTop = false;
+
   FastLED.setBrightness(100);
+
   for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
     leds[0][i] = CRGB(20, 0, 25);
   }
+
   if (!reachedTop) {
     position++;
-  }
-  else {
+  } else {
     position--;
   }
+
   if (position >= NUM_LEDS_PER_STRIP) {
     reachedTop = true;
   }
   if (position <= 0) {
     reachedTop = false;
   }
-  // FastLED.setBrightness(200);
+
+  FastLED.setBrightness(200);
   leds[0][position] = CRGB(20, 0, 25);
   FastLED.show();
 }
 void CUBEREQUEST4() {
   FastLED.setBrightness(100);
+
   for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
     leds[0][i] = CRGB(20, 0, 25);
   }
@@ -165,13 +230,14 @@ void CUBEREQUEST4() {
     leds[0][i].fadeToBlackBy(64);
   }
   for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-    leds[0][i].fadeLightBy(255);
+    leds[0][i].fadeLightBy(256);
   }
 }
 void CUBEAQUIRED()
 // Set all the LEDs in the Array to Purple
 {
   FastLED.setBrightness(100);
+
   for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
     leds[0][i] = CRGB(20, 0, 25);
     FastLED.show();
@@ -182,6 +248,7 @@ void DISABLED()
 // TODO: Check if the Color is the same as the disabled color on the robot, or at least close to it
 {
   FastLED.setBrightness(100);
+
   for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
     leds[0][i] = CRGB(255, 0, 0);
     FastLED.show();
@@ -191,6 +258,7 @@ void BLUEALLIANCE()
 // Set all the LEDs in the array to Blue
 {
   FastLED.setBrightness(100);
+
   for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
     leds[0][i] = CRGB(0, 0, 255);
     FastLED.show();
@@ -223,20 +291,24 @@ void BLUEALLIANCE()
 //         }
 //   FastLED.delay(250);
 // }
+
 void RANDOMLIGHTS() {
   FastLED.setBrightness(100);
-  
-    // Pick a random number from 0 to 255 and set each of the rgb values to one of the random valuesd
-    for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-     randNumber1 = random(0, 255);
+
+  // Pick a random number from 0 to 255 and set each of the rgb values to one of the random valuesd
+  for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+
+    randNumber1 = random(0, 255);
     randNumber2 = random(0, 225);
     randNumber3 = random(0, 225);
     leds[0][i] = CRGB(randNumber1, randNumber2, randNumber3);
-   FastLED.show();
+
+    FastLED.show();
   }
 }
 void AUTOBALANCEENGAGED() {
   FastLED.setBrightness(100);
+
   for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
     leds[0][i] = CRGB(255, 255, 255);
     FastLED.show();
@@ -245,39 +317,43 @@ void AUTOBALANCEENGAGED() {
 void AUTOBALANCENOTENGAGED() {
   static int position = 0;
   static bool reachedTop = false;
+
   FastLED.setBrightness(100);
+
   for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
     leds[0][i] = CRGB(255, 255, 255);
   }
+
   if (!reachedTop) {
     position++;
-  }
-  else {
+  } else {
     position--;
   }
+
   if (position >= NUM_LEDS_PER_STRIP) {
     reachedTop = true;
   }
   if (position <= 0) {
     reachedTop = false;
   }
-  // FastLED.setBrightness(200);
+
+  FastLED.setBrightness(200);
   leds[0][position] = CRGB(255, 255, 255);
   FastLED.show();
 }
 void loop() {
-  
-    // Initialize digital Read pins
+
+  // Initialize digital Read pins
   int pin2 = digitalRead(2);
   int pin3 = digitalRead(3);
   int pin4 = digitalRead(4);
   int pin5 = digitalRead(5);
-  
 
 
-    // If statements determining which pattern is being sent from the RIO w/ DIO
-    // Look at variable declaration of PIN_INACTIVE/PIN_ACTIVE for information
-    if (pin2 == PIN_INACTIVE && pin3 == PIN_INACTIVE && pin4 == PIN_INACTIVE && pin5 == PIN_INACTIVE) {
+
+  // If statements determining which pattern is being sent from the RIO w/ DIO
+  // Look at variable declaration of PIN_INACTIVE/PIN_ACTIVE for information
+  if (pin2 == PIN_INACTIVE && pin3 == PIN_INACTIVE && pin4 == PIN_INACTIVE && pin5 == PIN_INACTIVE) {
     DISABLED();
   }
   // else if(pin2 == PIN_ACTIVE && pin3 == PIN_ACTIVE && pin4 == PIN_ACTIVE && pin5 == PIN_ACTIVE){
@@ -285,32 +361,23 @@ void loop() {
   // }
   else if (pin2 == PIN_ACTIVE && pin3 == PIN_ACTIVE && pin4 == PIN_INACTIVE && pin5 == PIN_INACTIVE) {
     BLUEALLIANCE();
-  }
-  else if (pin2 == PIN_ACTIVE && pin3 == PIN_ACTIVE && pin4 == PIN_ACTIVE && pin5 == PIN_INACTIVE) {
+  } else if (pin2 == PIN_ACTIVE && pin3 == PIN_ACTIVE && pin4 == PIN_ACTIVE && pin5 == PIN_INACTIVE) {
     REDALLIANCE();
-  }
-  else if (pin2 == PIN_ACTIVE && pin3 == PIN_INACTIVE && pin4 == PIN_INACTIVE && pin5 == PIN_INACTIVE) {
+  } else if (pin2 == PIN_ACTIVE && pin3 == PIN_INACTIVE && pin4 == PIN_INACTIVE && pin5 == PIN_INACTIVE) {
     CONEREQUEST4();  //CONEREQUEST();
-  }
-  else if (pin2 == PIN_INACTIVE && pin3 == PIN_INACTIVE && pin4 == PIN_INACTIVE && pin5 == PIN_ACTIVE) {
+  } else if (pin2 == PIN_INACTIVE && pin3 == PIN_INACTIVE && pin4 == PIN_INACTIVE && pin5 == PIN_ACTIVE) {
     CONEAQUIRED();
-  }
-  else if (pin2 == PIN_INACTIVE && pin3 == PIN_ACTIVE && pin4 == PIN_INACTIVE && pin5 == PIN_INACTIVE) {
+  } else if (pin2 == PIN_INACTIVE && pin3 == PIN_ACTIVE && pin4 == PIN_INACTIVE && pin5 == PIN_INACTIVE) {
     CUBEREQUEST4();  //CUBEREQUEST();
-  }
-  else if (pin2 == PIN_INACTIVE && pin3 == PIN_INACTIVE && pin4 == PIN_ACTIVE && pin5 == PIN_INACTIVE) {
+  } else if (pin2 == PIN_INACTIVE && pin3 == PIN_INACTIVE && pin4 == PIN_ACTIVE && pin5 == PIN_INACTIVE) {
     CUBEAQUIRED();
-  }
-  else if (pin2 == PIN_INACTIVE && pin3 == PIN_INACTIVE && pin4 == PIN_ACTIVE && pin5 == PIN_ACTIVE) {
+  } else if (pin2 == PIN_INACTIVE && pin3 == PIN_INACTIVE && pin4 == PIN_ACTIVE && pin5 == PIN_ACTIVE) {
     RANDOMLIGHTS();
-  }
-  else if (pin2 == PIN_ACTIVE && pin3 == PIN_INACTIVE && pin4 == PIN_ACTIVE && pin5 == PIN_INACTIVE) {
+  } else if (pin2 == PIN_ACTIVE && pin3 == PIN_INACTIVE && pin4 == PIN_ACTIVE && pin5 == PIN_INACTIVE) {
     AUTOBALANCEENGAGED();
-  }
-  else if (pin2 == PIN_INACTIVE && pin3 == PIN_ACTIVE && pin4 == PIN_INACTIVE && pin5 == PIN_ACTIVE) {
+  } else if (pin2 == PIN_INACTIVE && pin3 == PIN_ACTIVE && pin4 == PIN_INACTIVE && pin5 == PIN_ACTIVE) {
     AUTOBALANCENOTENGAGED();
-  }
-  else {
+  } else {
     DISABLED();
   }
 }
